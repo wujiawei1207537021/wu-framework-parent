ce9728f64981c0d858d52629cd4ca4591d25d744
wujiawei <1207537021@qq.com>
2020-10-12 08:33:14 +0800

    删除enableswagger2 注解

--- wu-easy-parent/wu-easy-temple/src/main/java/com/wu/framework/easy/temple/EasyTempleApplication.java
+++ wu-easy-parent/wu-easy-temple/src/main/java/com/wu/framework/easy/temple/EasyTempleApplication.java
@@ -2,7 +2,6 @@ package com.wu.framework.easy.temple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author : 吴佳伟
@@ -10,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @describe :
  * @date : 2020/9/18 下午11:30
  */
-@EnableSwagger2
 @SpringBootApplication
 public class EasyTempleApplication {
     public static void main(String[] args) {
