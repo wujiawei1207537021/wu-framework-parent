80f3b9520c84a4867261c9a6004bf67dfaf6192b
wujiawei <1207537021@qq.com>
2021-01-21 19:38:08 +0800

    新增测试代码

--- wu-freamwork-test/src/main/java/com/wu/freamwork/controller/DataBaseTestController.java
+++ wu-freamwork-test/src/main/java/com/wu/freamwork/controller/DataBaseTestController.java
@@ -6,7 +6,6 @@ import com.wu.framework.easy.stereotype.upsert.entity.EasyHashMap;
 import com.wu.framework.easy.stereotype.upsert.process.MySQLDataProcess;
 import com.wu.framework.easy.stereotype.upsert.util.FileUtil;
 import com.wu.framework.easy.stereotype.web.EasyController;
-
 import com.wu.framework.inner.lazy.database.expand.database.persistence.LazyOperation;
 import com.wu.framework.inner.lazy.database.test.pojo.DataBaseUser;
 import org.springframework.boot.CommandLineRunner;
@@ -27,7 +26,7 @@ import java.util.List;
 @EasyController
 public class DataBaseTestController implements CommandLineRunner {
 
-    private final  LazyOperation layerOperation;
+    private final LazyOperation layerOperation;
     private final UpsertConfig upsertConfig;
     private final MySQLDataProcess mySQLDataProcess;
 
@@ -51,33 +50,7 @@ public class DataBaseTestController implements CommandLineRunner {
 //        List<String> ss = layerOperation.executeSQL("select id from user", String.class);
 //        System.out.println(ss);
 //        layerOperation.activeUpsert(new DataBaseUser().setAge(20));
-        // 获取数据库中所有的表
-        String sql = "select table_name tableName, engine, table_comment tableComment, create_time createTime from information_schema.tables where table_schema = (select database()) ";
-        List<EasyHashMap> easyHashMaps = layerOperation.executeSQL(sql, EasyHashMap.class);
-
-        BufferedWriter file = FileUtil.createFile(upsertConfig.getCacheFileAddress(), "hc升级sql脚本.sql");
-        for (EasyHashMap easyHashMap : easyHashMaps) {
-            String countSQL="select count(1) from %s ";
-            String tableName = easyHashMap.get("TABLE_NAME").toString();
-            Integer count = layerOperation.executeSQLForBean(String.format(countSQL, tableName.toString()), Integer.class);
-            if(count!=0){
-                String  selectSQL="select * from %s ";
-                List<EasyHashMap> tableDateList = layerOperation.executeSQL(String.format(selectSQL, tableName), EasyHashMap.class);
-                System.out.println(tableDateList);
-                file.write("-- "+ tableName);
-                file.newLine();
-                EasyHashMap easyHashMap1 = tableDateList.get(0);
-                easyHashMap1.setUniqueLabel(tableName);
-                String s = mySQLDataProcess.dataPack(tableDateList, easyHashMap1.toEasyTableAnnotation(false));
-                file.write(s);
-                file.write(";");
-                file.newLine();
-            }
-        }
-        file.close();
-
-        // 获取表中所有的数据
-        // 将数据转换为更新语句
+        hc();
     }
 
     /**
@@ -203,5 +176,72 @@ public class DataBaseTestController implements CommandLineRunner {
         System.out.println(dataBaseUserList);
         long e = System.currentTimeMillis();
         System.out.println("共计用时：" + (e - s) + "ms");
+    }
+
+
+    /**
+     * description hc系统升级数据同步
+     *
+     * @param
+     * @return
+     * @exception/throws
+     * @author 吴佳伟
+     * @date 2021/1/21 下午7:31
+     */
+    public void hc() throws Exception {
+        // 获取数据库中所有的表
+        String sql = "select table_name tableName, engine, table_comment tableComment, create_time createTime from information_schema.tables where table_schema = (select database()) ";
+        List<EasyHashMap> allTables = layerOperation.executeSQL(sql, EasyHashMap.class);
+
+        BufferedWriter file = FileUtil.createFile(upsertConfig.getCacheFileAddress(), "hc6.3.3升级sql脚本.sql");
+        for (EasyHashMap table : allTables) {
+            String countSQL = "select count(1) from %s ";
+            String tableName = table.get("TABLE_NAME").toString();
+            Integer count = layerOperation.executeSQLForBean(String.format(countSQL, tableName.toString()), Integer.class);
+            if (count != 0) {
+                String selectSQL = "select * from %s ";
+                List<EasyHashMap> tableDateList = layerOperation.executeSQL(String.format(selectSQL, tableName), EasyHashMap.class);
+
+
+                // ho_dictionary_data
+                if (tableName.equals("ho_dictionary_data")) {
+                    for (EasyHashMap easyHashMap : tableDateList) {
+                        Object code = easyHashMap.get("code");
+                        Object dictionaryCode = easyHashMap.get("dictionary_code");
+                        Object id = easyHashMap.get("id");
+                        easyHashMap.put("id", String.format("wujiaweiifnull((select T1.id from %s T1 where T1.code='%s' and T1.dictionary_code='%s'),%s)wujiawei", tableName, code, dictionaryCode, id));
+                    }
+                }
+
+                // 字典
+                // 用户操作表
+                if (tableName.equals("ho_operate") || tableName.equals("ho_dictionary")) {
+                    for (EasyHashMap easyHashMap : tableDateList) {
+                        Object code = easyHashMap.get("code");
+                        Object id = easyHashMap.get("id");
+                        easyHashMap.put("id", String.format("wujiaweiifnull((select T1.id from %s T1 where T1.code='%s'),%s)wujiawei", tableName, code, id));
+                    }
+                }
+                System.out.println(tableDateList);
+                file.write("-- " + tableName);
+                file.newLine();
+                EasyHashMap tableInfo = tableDateList.get(0);
+                tableInfo.setUniqueLabel(tableName);
+                String s = mySQLDataProcess.dataPack(tableDateList, tableInfo.toEasyTableAnnotation(false));
+                s = s.replaceAll("'true'", "1").
+                        replaceAll("'false'", "0").
+                        replaceAll("'null'", "null").
+                        replaceAll("'wujiawei", "").
+                        replaceAll("wujiawei'", "");
+
+                file.write(s);
+                file.write(";");
+                file.newLine();
+            }
+        }
+        file.close();
+
+        // 获取表中所有的数据
+        // 将数据转换为更新语句
     }
 }
