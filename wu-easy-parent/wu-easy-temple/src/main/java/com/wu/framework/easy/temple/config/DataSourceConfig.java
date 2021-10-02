04d10edbeff9a972e2f198f7b73a529610830e3c
wujiawei <1207537021@qq.com>
2021-10-02 11:42:44 +0800

    修改配置的多余的h2数据配置

--- wu-easy-parent/wu-easy-temple/src/main/java/com/wu/framework/easy/temple/config/DataSourceConfig.java
+++ wu-easy-parent/wu-easy-temple/src/main/java/com/wu/framework/easy/temple/config/DataSourceConfig.java
@@ -20,9 +20,9 @@ public class DataSourceConfig {
     @Bean(name = "dataSourceMySQL")
     public DataSource dataSourceMySQL() {
         MysqlDataSource build = DataSourceBuilder.create().type(MysqlDataSource.class).build();
-        build.setUrl("jdbc:mysql://192.168.17.221:30712/test?rewriteBatchedStatements=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/temp?rewriteBatchedStatements=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
         build.setUser("root");
-        build.setPassword("laihui");
+        build.setPassword("wujiawei");
         return build;
     }
 
@@ -34,7 +34,7 @@ public class DataSourceConfig {
      * @author Jia wei Wu
      * @date 2021/4/15 8:21 下午
      **/
-    @Bean(name = "dataSourceH2")
+//    @Bean(name = "dataSourceH2")
     public DataSource dataSourceH2() {
         JdbcDataSource build = DataSourceBuilder.create().type(org.h2.jdbcx.JdbcDataSource.class).build();
         build.setUrl("jdbc:h2:file:./database/db");
